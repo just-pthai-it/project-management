577e0dd3b1b036aba181d70eb9e25a3e96bc5c75
pthai <pthai.it.210501@gmail.com>
2023-05-14 22:49:18 +0700

    Update logic.

--- app/Http/Resources/Project/ProjectResource.php
+++ app/Http/Resources/Project/ProjectResource.php
@@ -29,7 +29,7 @@ class ProjectResource extends JsonResource
             'status'         => $this->status,
             'progress'       => $this->progress,
             'pending_reason' => $this->whenHas('progress'),
-            'users'          => $this->users,
+            'users'          => $this->whenLoaded('users'),
             'created_at'     => $this->created_at,
             'updated_at'     => $this->updated_at,
         ];
