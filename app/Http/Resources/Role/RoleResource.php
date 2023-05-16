9b86fc68b2e21feee5254d8db3f834c20eb73221
pthai <pthai.it.210501@gmail.com>
2023-05-16 19:54:32 +0700

    Update response of get list roles API.

---
+++ app/Http/Resources/Role/RoleResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Role;
+
+use App\Models\Role;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+class RoleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray ($request) : array|JsonSerializable|Arrayable
+    {
+        return [
+            'id'          => $this->id,
+            'name'        => $this->name,
+            'is_editable' => $this->name != Role::ROLE_ROOT_NAME,
+        ];
+    }
+}
