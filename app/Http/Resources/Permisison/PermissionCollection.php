3dd843678ec272801ae3cae345554b3b2bdec625
pthai <pthai.it.210501@gmail.com>
2023-05-21 14:43:40 +0700

    Update response of get list permissions API.

---
+++ app/Http/Resources/Permisison/PermissionCollection.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources\Permisison;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use JsonSerializable;
+
+class PermissionCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param Request $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray ($request) : array|JsonSerializable|Arrayable
+    {
+        $this->collection = $this->collection->groupBy('group_name');
+        $this->collection->transform(function ($item, $key)
+        {
+            $item->transform(function ($item, $key)
+            {
+                return [
+                    'value' => $item->id,
+                    'label' => $item->name,
+                ];
+            });
+
+            return [
+                'groupName'   => $key,
+                'permissions' => $item,
+            ];
+        });
+
+        return $this->collection->values();
+    }
+}
