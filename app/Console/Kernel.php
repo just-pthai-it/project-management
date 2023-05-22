da3802637518e178c6fb39d6ca870c9c837b6174
pthai <pthai.it.210501@gmail.com>
2023-05-23 02:19:49 +0700

    Add schedule to update behind schedule status of project and task.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Jobs\UpdateBehindScheduleProjectStatus;
+use App\Jobs\UpdateBehindScheduleTaskStatus;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -10,12 +12,14 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param Schedule $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule (Schedule $schedule) : void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new UpdateBehindScheduleProjectStatus())->timezone('Asia/Ho_Chi_Minh')->dailyAt('00:00');
+        $schedule->job(new UpdateBehindScheduleTaskStatus())->hourly();
+
     }
 
     /**
@@ -23,9 +27,9 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
+    protected function commands ()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
---
+++ app/Jobs/UpdateBehindScheduleProjectStatus.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Project;
+use App\Models\ProjectStatus;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateBehindScheduleProjectStatus implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct ()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle () : void
+    {
+        Project::query()->where('ends_at', '>', now('+7'))
+               ->update(['status_id' => ProjectStatus::STATUS_BEHIND_SCHEDULE]);
+
+    }
+}
